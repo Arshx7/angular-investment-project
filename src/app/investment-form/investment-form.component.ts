7c312c85f0a6fcda8ed18c79df04dcc5a4c5f410
Arshakh Haneen <arshx77@gmail.com>
2025-01-20 19:33:43 +0530

    Add calculation logic

--- src/app/investment-form/investment-form.component.ts
+++ src/app/investment-form/investment-form.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-investment-form',
   imports: [FormsModule],
   templateUrl: './investment-form.component.html',
-  styleUrl: './investment-form.component.css'
+  styleUrl: './investment-form.component.css',
 })
 export class InvestmentFormComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }>();
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
   onSubmit() {
-    console.log("hi");
-    
+    console.log('hi');
+    console.log(
+      this.enteredAnnualInvestment,
+      this.enteredInitialInvestment,
+      this.enteredDuration
+    );
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    });
   }
 }
